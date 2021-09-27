79cbbd84f30292032874a364029b40911efeabfe
jamh <jamh26@users.noreply.github.com>
2021-09-27 12:31:22 -0600

    feat: added routes for GetUsers, AddUser, GetUser

--- HealthNotebook.Api/Controllers/UsersController.cs
+++ HealthNotebook.Api/Controllers/UsersController.cs
@@ -1,3 +1,8 @@
+using System;
+using System.Linq;
+using HealthNotebook.DataService.Data;
+using HealthNotebook.Entities.DbSet;
+using HealthNotebook.Entities.Dtos.Incoming;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HealthNotebook.Api.Controllers
@@ -6,11 +11,49 @@ namespace HealthNotebook.Api.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
-        [Route("TestRun")]
-        [HttpGet]
-        public IActionResult TestRun()
+        private AppDbContext _context;
+
+        public UsersController(AppDbContext context)
         {
-            return Ok("Success");
+            _context = context;
+        }
+
+        // GET all users
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = _context.Users.Where(x => x.Status == 1).ToList();
+
+            return Ok(users);
+        }
+
+        // POST
+        [HttpPost]
+        public IActionResult AddUser(UserDto user)
+        {
+            var _user = new User();
+            _user.LastName = user.LastName;
+            _user.FirstName = user.FirstName;
+            _user.Email = user.Email;
+            _user.DateOfBirth = Convert.ToDateTime(user.DateOfBirth);
+            _user.Country = user.Country;
+            _user.Phone = user.Phone;
+            _user.Status =1;
+
+            _context.Users.Add(_user);
+            _context.SaveChanges();
+
+            return Ok(); // return a 201
+        }
+
+        // GET
+        [HttpGet]
+        [Route("GetUser")]
+        public IActionResult GetUser(Guid id )
+        {
+            var user = _context.Users.FirstOrDefault(x => x.Id == id);
+
+            return Ok(user);
         }
     }
 }
