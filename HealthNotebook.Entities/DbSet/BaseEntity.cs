72b60d09426dce38d86ea62560d2f13787f3684e
jamh <jamh26@users.noreply.github.com>
2021-09-27 10:29:40 -0600

    fix: typo in attribute name

--- HealthNotebook.Entities/DbSet/BaseEntity.cs
+++ HealthNotebook.Entities/DbSet/BaseEntity.cs
@@ -7,6 +7,6 @@ namespace HealthNotebook.Entities.DbSet
         public Guid Id { get; set; } => Guid.NewGuid();
         public int Status { get; set; } = 1;
         public DateTime AddedDate { get; set; } => DateTime.UtcNow;
-        public DateTime updateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
     }
 }
