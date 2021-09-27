004ab0dec759b0f77bbbff2b9b73fdf7858c13a0
jamh <jamh26@users.noreply.github.com>
2021-09-27 10:55:54 -0600

    chore: add ref's to projects and build

--- HealthNotebook.DataService/Data/AppDbContext.cs
+++ HealthNotebook.DataService/Data/AppDbContext.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using HealthNotebook.Entities.DbSet;
 
 namespace HealthNotebook.DataService.Data
 {
     public class AppDbContext : IdentityDbContext
     {
+        public virtual DbSet<User> Users { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
 
