7e4d6369e1a9eee37a0a09f14b411222e4ca21f3
jamh <jamh26@users.noreply.github.com>
2021-10-23 23:24:28 -0600

    feat: added auth to UsersController

--- HealthNotebook.Api/Controllers/v1/UsersController.cs
+++ HealthNotebook.Api/Controllers/v1/UsersController.cs
@@ -5,11 +5,13 @@ using HealthNotebook.DataService.Data;
 using HealthNotebook.DataService.IConfiguration;
 using HealthNotebook.Entities.DbSet;
 using HealthNotebook.Entities.Dtos.Incoming;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HealthNotebook.Api.Controllers.v1
 {
-
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     public class UsersController : BaseController
     {
         public UsersController(IUnitOfWork unitOfWork) : base(unitOfWork)
