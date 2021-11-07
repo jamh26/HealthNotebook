2a59410e9e1e36a5f70ea195181f869dc527d5e7
jamh <jamh26@users.noreply.github.com>
2021-11-07 06:22:36 -0700

    feat: updated user repo with method to update profile information

--- HealthNotebook.DataService/IRepository/IUsersRepository.cs
+++ HealthNotebook.DataService/IRepository/IUsersRepository.cs
@@ -1,9 +1,10 @@
+using System.Threading.Tasks;
 using HealthNotebook.Entities.DbSet;
 
 namespace HealthNotebook.DataService.IRepository
 {
     public interface IUsersRepository : IGenericRepository<User>
     {
-        
+        Task<bool> UpdateUserProfile(User user);
     }
 }
